d9517b939084ba24485f42795e3b4f3213283970
ob1-r7i <ob1-r7i@aisg.org>
2025-06-12 16:49:48 +0800

    prepare for d9_last veri

--- internvl_chat/rollout_generation/generated_rollouts/soft_estimation/RAVEN/verification/merge_and_map_batch.py
+++ internvl_chat/rollout_generation/generated_rollouts/soft_estimation/RAVEN/verification/merge_and_map_batch.py
@@ -394,7 +394,7 @@ def split_jsonl_into_batches(merged_file: str, batch_output_dir: str, lines_per_
 
 def main():
     # Configuration
-    split = "distribute_nine"
+    split = "distribute_nine_last"
     input_folder = f"/data/users/brandon/ob1-projects/InternVL/internvl_chat/rollout_generation/generated_rollouts/soft_estimation/RAVEN/final_output/{split}"
     output_dir = f"/data/users/brandon/ob1-projects/InternVL/internvl_chat/rollout_generation/generated_rollouts/soft_estimation/RAVEN/verification/verification_pipeline_outputs/{split}"
     merged_file = os.path.join(output_dir, "merged_batch_output.jsonl")
---
+++ internvl_chat/rollout_generation/generated_rollouts/soft_estimation/README.md
@@ -0,0 +1,6 @@
+1. Transfer all images and their absolute paths to preprocessing_scripts/{dataset_name}
+2. Specify the endpoints, deploymdent and config in rollout.py THEN ./run_rollout.sh to generate rollouts
+3. Transfer completed rollouts to generated_rollouts/soft_estimation/{dataset_name}/final_output/{split_name}
+4. run ./run_batch_processor.sh to verify the rollouts, checking for parameters in batch_processor.py
+    - use test_batch_processor.py to test the batch processor
+    - use test_timeout monitor to test recovery for stale Azure Batch Request
