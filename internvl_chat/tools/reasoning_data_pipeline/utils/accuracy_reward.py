99bda54765e29582d049b2fec11f77ae237e4638
ob1-r7i <ob1-r7i@aisg.org>
2025-06-22 04:59:49 +0800

    rollout 50% AI2D

--- internvl_chat/tools/reasoning_data_pipeline/utils/accuracy_reward.py
+++ internvl_chat/tools/reasoning_data_pipeline/utils/accuracy_reward.py
@@ -451,7 +451,7 @@ def ai2d_open_answer_score(answer_pred, answer_gt, image_path=None, question=Non
         
     client = AzureOpenAI(
         api_version="2025-01-01-preview",
-        azure_endpoint="https://decla-mbnd9fjk-switzerlandnorth.cognitiveservices.azure.com/",
+        azure_endpoint="https://decla-mbndz1ad-germanywestcentra.cognitiveservices.azure.com/",
         api_key=os.getenv("AZURE_CORRECTNESSJUDGE_API_KEY"),
         timeout=60.0
     )
