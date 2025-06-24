6864bf4607c8115b001612708d1f1ef359bc8b29
ob1-r7i <ob1-r7i@aisg.org>
2025-06-24 18:32:06 +0800

    AI2D rolled

--- internvl_chat/rollout_generation/generated_rollouts/soft_estimation/AI2D/rollout.py
+++ internvl_chat/rollout_generation/generated_rollouts/soft_estimation/AI2D/rollout.py
@@ -39,8 +39,8 @@ from reasoning_data_pipeline.utils.accuracy_reward import (check_answer, parse_a
 from reasoning_data_pipeline.utils.utils import localtime
 
 # Azure OpenAI Configuration
-endpoint = "https://decla-mbne4gph-southafricanorth.cognitiveservices.azure.com/"
-deployment = "gpt-4.1-16"
+endpoint = "https://decla-mbndd7fk-uksouth.cognitiveservices.azure.com/"
+deployment = "gpt-4.1-11"
 api_version = "2025-01-01-preview"
 
 client = AzureOpenAI(
@@ -931,8 +931,8 @@ args = {
     'out_dir': 'ai2d_open_answer_rollouts_output',
     'batch_size': 15,  # ~20 samples per batch
     'num_return_sequences': 4,  # 20×4 = 80 requests per batch (ensure this is FAST less than 20s so we are rate limited at the TPM level in phase 2)
-    'sample_start_idx': 5149,
-    'sample_end_idx': 5469,
+    'sample_start_idx': 6754,
+    'sample_end_idx': 7074,
     'prompt_format_version': 'dvqa_v1_int_only',
     'scoring_mode': 'ai2d_open_answer_score',
     'num_mc_sequences': 16,  # 16 MC sequences per rollout
